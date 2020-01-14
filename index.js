2d5b15532b0a9869fb34d24b07a9b5f07c89ced0
mark bashir <beejayphil@gmail.com>
2020-01-14 09:28:11 +0100

    google cloud function serverless

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const puppeteer = require('puppeteer');
 
-(async function getBunnies() { 
-  const browser = await puppeteer.launch({ headless: false });
+async function getBunnies() { 
+  const browser = await puppeteer.launch({ args:['--no-sandbox'] });
   const page = await browser.newPage();
   const url = 'https://nh.craigslist.org/search/sss?query=bunnies&sort=rel&haspic=1';
   await page.goto(url);
   await page.waitForSelector('.result-row');
-  const results = await page.$$eval('.result-row', rows => rows);
+  const results = await page.$$eval('.result-row', rows => { 
+    return rows.map(row => { 
+      const properties = {};
+      const titleElement = row.querySelector();
+      properties.title = titleElement.innerText;
+      properties.url = titleElement.getAttribute('href');
+      const priceElement = row.querySelector('.result-price');
+      properties.price = priceElement ? priceElement.innerText : '';
+      const imageEl = row.querySelector('.swipe [data-index=0] img');
+      properties.imageUrl = imageEl ? imageEl.src : '';
+      return properties;
+    });
+   
+
+
+  });
   console.log(results);
-})();
+  browser.close();
+  return {url,results};
+}
+
+exports.getBunnies = async function (req, res) {
+  return res.status(200).send(await getBunnies())
+}
