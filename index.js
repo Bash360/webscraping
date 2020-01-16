c2df44baf043a5ff67fd3363bbe2485d3f92518e
mark bashir <beejayphil@gmail.com>
2020-01-16 11:46:46 +0100

    clean up

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 const puppeteer = require('puppeteer');
 
-async function getBunnies() { 
-  const browser = await puppeteer.launch({ args:['--no-sandbox'] });
+(async function getBunnies() {
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const url = 'https://nh.craigslist.org/search/sss?query=bunnies&sort=rel&haspic=1';
   await page.goto(url);
   await page.waitForSelector('.result-row');
-  const results = await page.$$eval('.result-row', rows => { 
-    return rows.map(row => { 
+  const results = await page.$$eval('.result-row', rows => {
+    return rows.map(row => {
+    
       const properties = {};
-      const titleElement = row.querySelector();
+      const titleElement = row.querySelector('.result-title');
       properties.title = titleElement.innerText;
       properties.url = titleElement.getAttribute('href');
       const priceElement = row.querySelector('.result-price');
       properties.price = priceElement ? priceElement.innerText : '';
-      const imageEl = row.querySelector('.swipe [data-index=0] img');
+      const imageEl = row.querySelector('.swipe [data-index="0"] img');
       properties.imageUrl = imageEl ? imageEl.src : '';
       return properties;
     });
+  }
    
 
 
-  });
-  console.log(results);
-  browser.close();
-  return {url,results};
-}
 
-exports.getBunnies = async function (req, res) {
-  return res.status(200).send(await getBunnies())
-}
+  );
+
+  browser.close();
+  return { url, results };
+})();
+
+// exports.getBunnies = async function (req, res) {
+//   return res.status(200).send(await getBunnies())
+// }
